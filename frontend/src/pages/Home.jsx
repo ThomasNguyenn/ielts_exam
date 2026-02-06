280354d8a9cc149a8447f20f4796798f50896498
ThomasNguyenn <nminhduc843@gmail.com>
2026-02-06 14:42:09 +0700

    Update file name

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Banner from '../components/banner';
+import Banner from '../components/Banner';
 import './Home.css';
 import BannerTwo from '../components/BannerTwo';
 
